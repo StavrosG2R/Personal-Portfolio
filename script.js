2ff30e02b4bada6aa7f4f034fb0c0b5bab57f71e
StavrosG2R <stavros.soudris@gmail.com>
2021-12-25 14:01:05 +0200

    Update script.js
    Validation added

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ links.forEach((link, i) => {
         }
     })
 })
+
+// Validation
+
+$(document).ready(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='contact-form']").validate({
+      // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            name: "required",
+            msg: "required",
+            email: {
+                required: true,
+            // Specify that email should be validated
+            // by the built-in "email" rule
+                email: true
+            },
+        },
+      // Specify validation error messages
+        messages: {
+            name: "Please enter your name",
+            msg: "Please fill the form",
+            email: "Please enter a valid email address"
+        },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
