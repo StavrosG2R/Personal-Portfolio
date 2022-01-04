79f05eb267529e67ac722f557c2d0a5c51339b29
StavrosG2R <stavros.soudris@gmail.com>
2022-01-04 12:26:07 +0200

    Update script.js
    Translate function added

--- script.js
+++ script.js
@@ -38,16 +38,23 @@ $(document).ready(function() {
                 email: true
             },
         },
-      // Specify validation error messages
+         // Specify validation error messages
         messages: {
             name: "Please enter your name",
             msg: "Please fill the form",
             email: "Please enter a valid email address"
         },
-      // Make sure the form is submitted to the destination defined
-      // in the "action" attribute of the form when valid
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
         submitHandler: function(form) {
             form.submit();
         }
     });
 });
+
+// Translate
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement ({
+        pageLanguage: 'en'}, 'google_translate_element');
+}
